7018c39b498b7c389f4eb43a3d1ec668babc0d52
fomuk <fomuk>
2015-11-30 20:31:15 +0300

    Initial commit

---
+++ src/com/fomuk/Main.java
@@ -0,0 +1,16 @@
+package com.fomuk;
+
+/**
+ * Created by fomuk on 29.11.2015.
+ */
+public class Main {
+
+    static final int CONST = 77;
+
+    public static void main(String[] args) {
+        System.out.println(CONST);
+        System.out.println(CONST);
+
+    }
+
+}
